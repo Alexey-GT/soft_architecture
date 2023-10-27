10f39e0af1f53ee2ae5b5d32bcc4dba54298879a
Alexey <Alexey.gt@bk.ru>
2023-10-27 12:24:06 +0300

    Homework_4

---
+++ HomeWork_4/core/ticketProvider.py
@@ -0,0 +1,14 @@
+from HomeWork_4.models.ticket import Ticket
+from HomeWork_4.services.ticketRepo import TicketRepo
+
+
+class TicketProvider:
+    def __init__(self):
+        self.ticketRepo = TicketRepo()
+        self.ticketRepo.repo_generation()
+
+    def getTickets(self, routeNumber):
+        return self.ticketRepo.readAll(routeNumber)
+
+    def updateTicketStatus(self, ticket: Ticket):
+        self.ticketRepo.update(ticket)
